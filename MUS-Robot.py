3f84cc373df76ff2c1e9678ebe93dcd80e47a16a
Thanos_Apollo <93195432+ThanosApollo@users.noreply.github.com>
2022-02-10 10:58:34 +0200

    Add files via upload

---
+++ MUS-Robot.py
@@ -0,0 +1,170 @@
+from telebot.callback_data import CallbackData, CallbackDataFilter
+from telebot import types, TeleBot
+from telebot.custom_filters import AdvancedCustomFilter
+from API_Tokens import *
+from Contactinfo import *
+
+API_TOKEN = Testing_bot
+
+bot = TeleBot(API_TOKEN)
+products_factory = CallbackData('product_id', prefix='products')
+
+
+def year1_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year1
+        ]
+    )
+
+def year2_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year2
+        ]
+    )
+
+def year3_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year3
+        ]
+    )
+
+def year4_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year4
+        ]
+    )
+def year5_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year5
+        ]
+    )
+def year6_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in year6
+        ]
+    )
+
+def dean_keyboard():
+    return types.InlineKeyboardMarkup(
+        keyboard=[
+            [
+                types.InlineKeyboardButton(
+                    text=subject['name'],
+                    callback_data=products_factory.new(product_id=subject["id"])
+                )
+            ]
+            for subject in dean
+        ]
+    )
+
+
+
+class SubjectsCallbackFilter(AdvancedCustomFilter):
+    key = 'config'
+
+    def check(self, call: types.CallbackQuery, config: CallbackDataFilter):
+        return config.check(query=call)
+
+@bot.message_handler(commands=['year1info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year1_keyboard())
+
+@bot.message_handler(commands=['year2info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year2_keyboard())
+
+@bot.message_handler(commands=['year3info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year3_keyboard())
+
+@bot.message_handler(commands=['year4info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year4_keyboard())
+
+@bot.message_handler(commands=['year5info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year5_keyboard())
+
+@bot.message_handler(commands=['year6info'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=year6_keyboard())
+
+@bot.message_handler(commands=['dean'])
+def products_command_handler(message: types.Message):
+    bot.send_message(message.chat.id, 'Subjects:', reply_markup=dean_keyboard())
+
+
+
+
+@bot.callback_query_handler(func=None, config=products_factory.filter())
+def products_callback(call: types.CallbackQuery):
+    callback_data: dict = products_factory.parse(callback_data=call.data)
+    product_id = int(callback_data['product_id'])
+    subject = subjects_all[product_id]
+
+    text = f"{subject['email']}"
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          text=text, )
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'back')
+def back_callback(call: types.CallbackQuery):
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                          text='Subjects:', )
+@bot.message_handler(commands=['library'])
+def send_library(message):
+    bot.send_message(message.chat.id, """
+https://drive.google.com/drive/u/0/folders/1jwAcnVtDQwHH2yQgbClMMBzmh85gBarW \nUse your university email to login, if you have any additional resources you wihs to be added let me know, educational resources should be accessible for everyone :)
+""")
+
+@bot.message_handler(commands=['info'])
+def send_library(message):
+    bot.send_message(message.chat.id, """
+    Hello, you can find the source code of MUS-Robot on my github account: https://github.com/ThanosApollo/MUS-Bot
+""")
+
+
+bot.add_custom_filter(SubjectsCallbackFilter())
+bot.infinity_polling()
