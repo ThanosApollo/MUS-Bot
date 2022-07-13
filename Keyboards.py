cb9b4ecd5fc67905dcf88f61f51128ba830a2e12
Thanos Apollo <apollothanosmd@gmail.com>
2022-07-13 15:01:43 +0300

    Main keyboard class and add todos

--- Keyboards.py
+++ Keyboards.py
@@ -9,94 +9,95 @@ from Contactinfo import *
 products_factory = CallbackData('product_id', prefix='products')
 
 
-def year1_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+class Keyboard:
+    def year1():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year1
             ]
-            for subject in year1
-        ]
-    )
+        )
 
-def year2_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+    def year2():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year2
             ]
-            for subject in year2
-        ]
-    )
+        )
 
-def year3_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+    def year3():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year3
             ]
-            for subject in year3
-        ]
-    )
+        )
 
-def year4_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+    def year4():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year4
             ]
-            for subject in year4
-        ]
-    )
-def year5_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+        )
+    def year5():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year5
             ]
-            for subject in year5
-        ]
-    )
-def year6_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+        )
+    def year6():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in year6
             ]
-            for subject in year6
-        ]
-    )
+        )
 
-def dean_keyboard():
-    return types.InlineKeyboardMarkup(
-        keyboard=[
-            [
-                types.InlineKeyboardButton(
-                    text=subject['name'],
-                    callback_data=products_factory.new(product_id=subject["id"])
-                )
+    def dean_keyboard():
+        return types.InlineKeyboardMarkup(
+            keyboard=[
+                [
+                    types.InlineKeyboardButton(
+                        text=subject['name'],
+                        callback_data=products_factory.new(product_id=subject["id"])
+                    )
+                ]
+                for subject in dean
             ]
-            for subject in dean
-        ]
-    )
+        )
 
 class SubjectsCallbackFilter(AdvancedCustomFilter):
     key = 'config'
